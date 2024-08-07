4bc05cb5a96f8f4e8c19f5abd739ede73cdc2622
hcsoylu <hcsoylu.dev@gmail.com>
2024-08-07 10:37:13 +0300

    add common navigation links

---
+++ src/lib/navLinks.ts
@@ -0,0 +1,14 @@
+import { Bot, TrendingUp } from 'lucide-react'
+
+export const navLinks = [
+  {
+    href: '/',
+    name: 'Agent Status',
+    icon: Bot,
+  },
+  {
+    href: '/statistics',
+    name: 'Statistics',
+    icon: TrendingUp,
+  },
+]
